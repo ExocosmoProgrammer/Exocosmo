d3920122c864160263a18fe52c51b5a0d3a394e7
ExocosmoProgrammer <gooberpea1024@gmail.com>
2023-08-18 16:11:04 -0700

    Update DemonstrationOneMainFile.py

--- DemonstrationOneMainFile.py
+++ DemonstrationOneMainFile.py
@@ -2,3 +2,18 @@ from DemonstrationOneClassFile import number
 print(__file__[41:], 'demonstrationMainFile')
 demonstrator = number()
 demonstrator.write()
+
+
+class otherNumber:
+    def __init__(self):
+        if __file__[41:] == 'DemonstrationOneMainFile.py':
+            self.value = 1
+        else:
+            self.value = 2
+
+    def write(self):
+        print(self.value)
+
+
+otherDemonstrator = otherNumber()
+otherDemonstrator.write()
